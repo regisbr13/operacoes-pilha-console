5eda6bbf6cc0d2315e5cc03bdcfa7ba6451b92d0
Regis Lima <regislima1391@gmail.com>
2019-02-20 13:46:15 -0300

    Implementado menu interativo

--- Pilha/Pilha.cpp
+++ Pilha/Pilha.cpp
@@ -1,21 +1,44 @@
-// Pilha.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    
+	int opcao = 1;
+
+	while (opcao != 0) {
+
+		cout << "####### Operacoes com Pilha #######\n \n";
+		cout << "|||||||||||||||||||||||||||||||||||||\n\n";
+		cout << "MENU\n\n";
+
+		cout << "0 - Sair \n";
+		cout << "1 - Empilhar \n";
+		cout << "2 - Exibir topo \n";
+		cout << "3 - Mostrar pilha \n";
+		cout << "4 - Desempilhar \n";
+		cout << "\n";
+		cout << "Opcao: ";
+		cin >> opcao;
+
+		if (opcao == 0) {
+			cout << "Fim do programa";
+		}
+		if (opcao == 1) {
+		}
+		if (opcao == 2) {
+		}
+		if (opcao == 3) {
+		}
+		if (opcao == 4) {
+		}
+		if (opcao > 4) {
+			cout << "Opçao Invalida";
+		}
+		system("PAUSE");
+		system("cls");
+	}
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
